430874ca16ae0f09e41e1fe03f3d4f6666e2a607
Henry <mbahenryemeka@gmail.com>
2024-05-30 18:07:39 -0400

    update comments, browser sends a post to the sever, server logs the request body to the console then respond with 200 ok.

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
@@ -10,55 +10,39 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
-app.use (bodyParser.urlencoded({extended: true}));
+// middleware to convert the request body into string that we can read.
 app.use(express.urlencoded({extended: true}));
 
+//  GET route to render the urls_new.ejs template.
 app.get('/urls/new', (req, res) =>{
   res.render('urls_new');
-})
-
-app.get('/urls/:id', (req, res) => {
-  const myID = req.params.id
-  const templateVars = {id: myID, longURL: urlDatabase[myID]}
-  res.render('urls_shows', templateVars);
 });
 
-
-
-
+// GET route to render the urls_index.ejs template.
 app.get('/urls', (req, res) => {
   const templateVars = {urls: urlDatabase};
-  res.render('urls_index', templateVars)
+  res.render('urls_index', templateVars);
 });
 
+// GET route to render urlDatabase in json format.
 app.get('/urls.json', (req, res) =>{
   res.json(urlDatabase);
 });
 
+// GET route to render the html format to the browser.
 app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-function generateRandomString() {
-  let result = '';
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const charactersLength = characters.length;
-  for (let i = 0; i<6; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
-}
 
 
+//  POST route to handle the form submission.
 app.post('/urls', (req, res) =>{
-  const shortURL = generateRandomString();  //  function to generate shortURL.
-  const longURL = req.body.longURL;
-  urlDatabase[shortURL] = longURL;  //  save the key-value pair in the database.
-  res.redirect(`/urls/${shortURL}`); // redirect to the path where the short URL can be viewed. 
-  });
+  console.log(req.body);
+  res.status(200).send('OK');
+});
 
-
-
+//  GET route for the root path to send greetings to the browser.
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
